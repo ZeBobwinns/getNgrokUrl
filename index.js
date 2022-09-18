f5461ed1f050fae80475835d54aa1943d0b95b47
ZeBobwinns <34847317+ZeBobwinns@users.noreply.github.com>
2022-09-18 18:08:53 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-var fileSystem = require("fs");
-const ngrok = require('ngrok');
-const api = ngrok.getApi();
-
-(async function() {
-    const url = await ngrok.connect();
-    console.log(url);
-
-    fileSystem.writeFile('ngrokURL.txt', url, err => {
-    if (err) {
-    console.error(err)
-    return
-    }
-    console.log("Wrote URL to ngrokURL.txt");
-})
-  })();
-
+const NgrokFreenom = require("@sundawning/ngrok-freenom-redirect");
+NgrokFreenom.start({
+  ngrok: {
+    token: "1fswFeNP8ZW0CxbreF0x663kuxf_2thUXdi3mitTtUKQoi1m",
+    port: 80,
+  },
+  freenom: {
+    username: "cazwinns@gmail.com",
+    password: "caz123caz123",
+    domain: "daboistag.ml",
+  },
+});
