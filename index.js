b22f71d6e9f580c3bf7af304327be8c9c02f0765
ZeBobwinns <34847317+ZeBobwinns@users.noreply.github.com>
2022-09-18 22:00:33 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let method = async () => {
+const url = await ngrok.connect({
+  proto: 'http',
+  addr: 80,
+  authtoken: '1fswFeNP8ZW0CxbreF0x663kuxf_2thUXdi3mitTtUKQoi1m'
+});
+}
 const NgrokFreenom = require("@sundawning/ngrok-freenom-redirect");
 NgrokFreenom.start({
   ngrok: {
